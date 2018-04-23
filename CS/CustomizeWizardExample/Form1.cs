b6300ca98f6df59da9248850bd508a9a21125a8f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomizeWizardExample/Form1.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Snap.Services;
+
+namespace CustomizeWizardExample
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1() {
+            InitializeComponent();
+            snapControl.Text = "Invoke the customized Data Source Wizard by clicking the Add New Data Source button on the File tab.";
+
+            #region #RegisterService
+            // Register the service for the Data Source Wizard customization.   
+            snapControl.AddService(typeof(IDataSourceWizardCustomizationService), new DataSourceWizardCustomizationService());
+            #endregion #RegisterService
+        }
+    }
+}
